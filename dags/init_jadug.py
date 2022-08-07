18c080cbec0cc20a79cb91d49e3afac742d32bf3
Jadug Destian <jdestian@gmail.com>
2022-08-08 06:31:49 +0700

    fixing error on init & daily dag, add other tables

--- dags/init_jadug.py
+++ dags/init_jadug.py
@@ -15,26 +15,30 @@ with DAG('init_jadug',
         task_id='start'
     )
 
-for table in ['orders']:    
+for table in ['orders', 'order_details', 'products']:    
       
     ingest = BashOperator(
         task_id='ingest_' + table,
-        bash_command="""python3 /root/airflow/dags/ingest/jadug/ingest_{{table}}.py {{ execution_date.format('YYYY-MM-DD') }}"""
+        bash_command="""python3 /root/airflow/dags/ingest/jadug/ingest_{{ params.table_name }}.py {{ execution_date.format('YYYY-MM-DD') }}""",
+        params={'table_name':table}
     )
 
     to_datalake = BashOperator(
         task_id='to_datalake_' + table,
-        bash_command="""gsutil cp /root/output/jadug/{{table}}/{{table}}_{{ execution_date.format('YYYY-MM-DD') }}.csv gs://digitalskola-de-batch7/jadug/staging/{{table}}/"""
+        bash_command="""gsutil cp /root/output/jadug/{{ params.table_name }}/{{ params.table_name }}_{{ execution_date.format('YYYY-MM-DD') }}.csv gs://digitalskola-de-batch7/jadug/staging/{{ params.table_name }}/""",
+        params={'table_name':table}
     )
 
     data_definition = BashOperator(
         task_id='data_definition_' + table,
-        bash_command="""bq mkdef --autodetect --source_format=CSV gs://digitalskola-de-batch7/jadug/staging/{{table}}/* > /root/table_def/jadug/{{table}}.def"""
+        bash_command="""bq mkdef --autodetect --source_format=CSV gs://digitalskola-de-batch7/jadug/staging/{{ params.table_name }}/* > /root/table_def/jadug/{{ params.table_name }}.def""",
+        params={'table_name':table}
     )
 
     to_dwh = BashOperator(
         task_id='to_dwh_orders',
-        bash_command="""bq mk --external_table_definition=/root/table_def/jadug/{{table}}.def de_7.jadug_{{table}}"""
+        bash_command="""bq mk --external_table_definition=/root/table_def/jadug/{{ params.table_name }}.def de_7.jadug_{{ params.table_name }}""",
+        params={'table_name':table}
     )
 
     start >> ingest >> to_datalake >> data_definition >> to_dwh

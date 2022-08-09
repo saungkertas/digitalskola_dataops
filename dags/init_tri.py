247f5862ac8aee6cae4a77749fec5e5236125141
Tri Purnomo <emailetri@gmail.com>
2022-08-09 14:12:52 +0700

    Update init_tri.py

--- dags/init_tri.py
+++ dags/init_tri.py
@@ -21,7 +21,7 @@ with DAG('init_tri',
         bash_command="""python3 /root/airflow/dags/ingest/tri/ingest_orders.py {{ execution_date.format('YYYY-MM-DD') }}"""
     )
 
-    ingest_order_detail = BashOperator(
+    ingest_order_details = BashOperator(
         task_id='ingest_order_details',
         bash_command="""python3 /root/airflow/dags/ingest/tri/ingest_orders_detail.py {{ execution_date.format('YYYY-MM-DD') }}"""
     )

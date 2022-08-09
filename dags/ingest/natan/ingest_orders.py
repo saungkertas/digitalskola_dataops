7d4c5a8efc0bcf8076ac59d350a562506f691bcc
Natanael136 <107261489+Natanael136@users.noreply.github.com>
2022-08-09 22:58:07 +0800

    Add files via upload

--- dags/ingest/natan/ingest_orders.py
+++ dags/ingest/natan/ingest_orders.py
@@ -10,7 +10,7 @@ psql_password = Variable.get("psql_password")
 psql_db = Variable.get("psql_db")
 
 conn = None
-sql = """select order_id,customer_id,employee_id,order_date,required_date,shipped_date,ship_country from orders o where cast(order_date as date) = '"""+sys.argv[1]+"""'"""
+sql = """select * from orders where cast(order_date as date) = '"""+sys.argv[1]+"""'"""
 csv_file_path = '/root/output/natan/orders/orders_'+sys.argv[1]+'.csv'
 
 try:

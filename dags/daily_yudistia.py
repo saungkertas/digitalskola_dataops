f55aa0ebf434f54fc77ef2236205799e1ae2ef27
distyoh <ydstia.adnan@gmail.com>
2022-08-10 06:40:50 +0700

    change schedules

--- dags/daily_yudistia.py
+++ dags/daily_yudistia.py
@@ -7,7 +7,7 @@ from airflow.utils.dates import days_ago
 from datetime import datetime, timedelta
 
 with DAG('daily_yudistia',
-    schedule_interval='0 0 * * *',
+    schedule_interval='@daily',
     start_date=datetime(2022, 7, 1)       
 ) as dag:
 

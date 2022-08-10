0b62c114691705285bd33395b67edefb8982aa16
shofadzik <107288295+shofadzik@users.noreply.github.com>
2022-08-10 13:23:35 +0700

    init & daily shofa

---
+++ dags/init_shofa.py
@@ -0,0 +1,132 @@
+from airflow import DAG
+from airflow.operators.dummy import DummyOperator
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+from airflow.utils.dates import days_ago
+
+from datetime import datetime, timedelta
+
+with DAG('init_shofa',
+    schedule_interval="@once",
+    start_date=datetime(2022, 7, 6)       
+) as dag:
+
+    start = DummyOperator(
+        task_id='start'
+    )    
+      
+    # orders
+
+    ingest_orders = BashOperator(
+        task_id='ingest_orders',
+        bash_command="""python3 /root/airflow/dags/ingest/shofa/ingest_orders.py {{ execution_date.format('YYYY-MM-DD') }}"""
+    )
+
+    to_datalake_orders = BashOperator(
+        task_id='to_datalake_orders',
+        bash_command="""gsutil cp /root/output/shofa/orders/orders_{{ execution_date.format('YYYY-MM-DD') }}.csv gs://digitalskola-de-batch7/shofa/staging/orders/"""
+    )
+
+    data_definition_orders = BashOperator(
+        task_id='data_definition_orders',
+        bash_command="""bq mkdef --autodetect --source_format=CSV gs://digitalskola-de-batch7/shofa/staging/orders/* > /root/table_def/shofa/orders.def"""
+    )
+
+    to_dwh_orders = BashOperator(
+        task_id='to_dwh_orders',
+        bash_command="""bq mk --external_table_definition=/root/table_def/shofa/orders.def de_7.shofa_orders"""
+    )
+
+    # order_details
+
+    ingest_order_details = BashOperator(
+        task_id='ingest_order_details',
+        bash_command="""python3 /root/airflow/dags/ingest/shofa/ingest_order_details.py {{ execution_date.format('YYYY-MM-DD') }}"""
+    )
+
+    to_datalake_order_details = BashOperator(
+        task_id='to_datalake_order_details',
+        bash_command="""gsutil cp /root/output/shofa/orders/order_details_{{ execution_date.format('YYYY-MM-DD') }}.csv gs://digitalskola-de-batch7/shofa/staging/order_details/"""
+    )
+
+    data_definition_order_details = BashOperator(
+        task_id='data_definition_order_details',
+        bash_command="""bq mkdef --autodetect --source_format=CSV gs://digitalskola-de-batch7/shofa/staging/order_details/* > /root/table_def/shofa/order_details.def"""
+    )
+
+    to_dwh_order_details = BashOperator(
+        task_id='to_dwh_order_details',
+        bash_command="""bq mk --external_table_definition=/root/table_def/shofa/order_details.def de_7.shofa_order_details"""
+    )
+
+    # products
+
+    ingest_products = BashOperator(
+        task_id='ingest_products',
+        bash_command="""python3 /root/airflow/dags/ingest/shofa/ingest_products.py {{ execution_date.format('YYYY-MM-DD') }}"""
+    )
+
+    to_datalake_products = BashOperator(
+        task_id='to_datalake_products',
+        bash_command="""gsutil cp /root/output/shofa/products/products_{{ execution_date.format('YYYY-MM-DD') }}.csv gs://digitalskola-de-batch7/shofa/staging/products/"""
+    )
+
+    data_definition_products = BashOperator(
+        task_id='data_definition_products',
+        bash_command="""bq mkdef --autodetect --source_format=CSV gs://digitalskola-de-batch7/shofa/staging/products/* > /root/table_def/shofa/products.def"""
+    )
+
+    to_dwh_products = BashOperator(
+        task_id='to_dwh_products',
+        bash_command="""bq mk --external_table_definition=/root/table_def/shofa/products.def de_7.shofa_products"""
+    )    
+
+    # categories
+
+    ingest_categories = BashOperator(
+        task_id='ingest_categories',
+        bash_command="""python3 /root/airflow/dags/ingest/shofa/ingest_categories.py {{ execution_date.format('YYYY-MM-DD') }}"""
+    )
+
+    to_datalake_categories = BashOperator(
+        task_id='to_datalake_categories',
+        bash_command="""gsutil cp /root/output/shofa/categories/categories_{{ execution_date.format('YYYY-MM-DD') }}.csv gs://digitalskola-de-batch7/shofa/staging/categories/"""
+    )
+
+    data_definition_categories = BashOperator(
+        task_id='data_definition_categories',
+        bash_command="""bq mkdef --autodetect --source_format=CSV gs://digitalskola-de-batch7/shofa/staging/categories/* > /root/table_def/shofa/categories.def"""
+    )
+
+    to_dwh_categories = BashOperator(
+        task_id='to_dwh_categories',
+        bash_command="""bq mk --external_table_definition=/root/table_def/shofa/categories.def de_7.shofa_categories"""
+    )
+
+    # suppliers
+
+    ingest_suppliers = BashOperator(
+        task_id='ingest_suppliers',
+        bash_command="""python3 /root/airflow/dags/ingest/shofa/ingest_suppliers.py {{ execution_date.format('YYYY-MM-DD') }}"""
+    )
+
+    to_datalake_suppliers = BashOperator(
+        task_id='to_datalake_suppliers',
+        bash_command="""gsutil cp /root/output/shofa/suppliers/suppliers_{{ execution_date.format('YYYY-MM-DD') }}.csv gs://digitalskola-de-batch7/shofa/staging/suppliers/"""
+    )
+
+    data_definition_suppliers = BashOperator(
+        task_id='data_definition_suppliers',
+        bash_command="""bq mkdef --autodetect --source_format=CSV gs://digitalskola-de-batch7/shofa/staging/suppliers/* > /root/table_def/shofa/suppliers.def"""
+    )
+
+    to_dwh_suppliers = BashOperator(
+        task_id='to_dwh_suppliers',
+        bash_command="""bq mk --external_table_definition=/root/table_def/shofa/suppliers.def de_7.shofa_suppliers"""
+    )
+
+    start >> ingest_orders >> to_datalake_orders >> data_definition_orders >> to_dwh_orders
+    start >> ingest_order_details >> to_datalake_order_details >> data_definition_order_details >> to_dwh_order_details
+    start >> ingest_products >> to_datalake_products >> data_definition_products >> to_dwh_products
+    start >> ingest_categories >> to_datalake_categories >> data_definition_categories >> to_dwh_categories
+    start >> ingest_suppliers >> to_datalake_suppliers >> data_definition_suppliers >> to_dwh_suppliers

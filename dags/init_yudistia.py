c329401c4ca0d0826c8216eccc780b444f8c2c07
distyoh <ydstia.adnan@gmail.com>
2022-08-10 19:23:10 +0700

    update new ingest data

--- dags/init_yudistia.py
+++ dags/init_yudistia.py
@@ -21,11 +21,6 @@ with DAG('init_yudistia',
         bash_command="""python3 /root/airflow/dags/ingest/yudistia/ingest_orders.py {{ execution_date.format('YYYY-MM-DD') }}"""
     )
 
-    ingest_orders_details = BashOperator(
-        task_id='ingest_orders_details',
-        bash_command="""python3 /root/airflow/dags/ingest/yudistia/ingest_orders_details.py {{ execution_date.format('YYYY-MM-DD') }}"""
-    )
-
     ingest_products = BashOperator(
         task_id='ingest_products',
         bash_command="""python3 /root/airflow/dags/ingest/yudistia/ingest_products.py {{ execution_date.format('YYYY-MM-DD') }}"""
@@ -47,16 +42,10 @@ with DAG('init_yudistia',
     )
 
 
-
     # To Data Lake
     to_datalake_orders = BashOperator(
         task_id='to_datalake_orders',
         bash_command="""gsutil cp /root/output/yudistia/orders/orders_{{ execution_date.format('YYYY-MM-DD') }}.csv gs://digitalskola-de-batch7/yudistia/staging/orders/"""
-    )
-
-    to_datalake_orders_details = BashOperator(
-        task_id='to_datalake_orders_details',
-        bash_command="""gsutil cp /root/output/yudistia/orders_details/orders_details_{{ execution_date.format('YYYY-MM-DD') }}.csv gs://digitalskola-de-batch7/yudistia/staging/orders_details/"""
     )
 
     to_datalake_products = BashOperator(
@@ -79,17 +68,10 @@ with DAG('init_yudistia',
         bash_command="""gsutil cp /root/output/yudistia/customer/customers_{{ execution_date.format('YYYY-MM-DD') }}.csv gs://digitalskola-de-batch7/yudistia/staging/customers/"""
     )
 
-
-
     #Data Definition
     data_definition_orders = BashOperator(
         task_id='data_definition_orders',
         bash_command="""bq mkdef --autodetect --source_format=CSV gs://digitalskola-de-batch7/yudistia/staging/orders/* > /root/table_def/yudistia/orders.def"""
-    )
-
-    data_definition_orders_details = BashOperator(
-        task_id='data_definition_orders_details',
-        bash_command="""bq mkdef --autodetect --source_format=CSV gs://digitalskola-de-batch7/yudistia/staging/orders_details/* > /root/table_def/yudistia/orders_details.def"""
     )
 
     data_definition_products = BashOperator(
@@ -112,17 +94,10 @@ with DAG('init_yudistia',
         bash_command="""bq mkdef --autodetect --source_format=CSV gs://digitalskola-de-batch7/yudistia/staging/customers/* > /root/table_def/yudistia/customers.def"""
     )
 
-
-
     #To Dwh
     to_dwh_orders = BashOperator(
         task_id='to_dwh_orders',
         bash_command="""bq mk --external_table_definition=/root/table_def/yudistia/orders.def de_7.yudistia_orders"""
-    )
-
-    to_dwh_orders_details = BashOperator(
-        task_id='to_dwh_orders_details',
-        bash_command="""bq mk --external_table_definition=/root/table_def/yudistia/orders_details.def de_7.yudistia_orders_details"""
     )
 
     to_dwh_products = BashOperator(
@@ -146,7 +121,6 @@ with DAG('init_yudistia',
     )
 
     start >> ingest_orders >> to_datalake_orders >> data_definition_orders >> to_dwh_orders
-    start >> ingest_orders_details >> to_datalake_orders_details >> data_definition_orders_details >> to_dwh_orders_details
     start >> ingest_products >> to_datalake_products >> data_definition_products >> to_dwh_products
     start >> ingest_suppliers >> to_datalake_suppliers >> data_definition_suppliers >> to_dwh_suppliers
     start >> ingest_categories >> to_datalake_categories >> data_definition_categories >> to_dwh_categories

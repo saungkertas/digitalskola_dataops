e860b0d9e17686ec64a0ac51274a236726ad64a4
ISBAKHUL LAIL <91885498+isbakhul21@users.noreply.github.com>
2022-08-10 22:41:25 +0700

    Update init_isbakhul_finalproject.py

--- dags/init_isbakhul_finalproject.py
+++ dags/init_isbakhul_finalproject.py
@@ -6,7 +6,7 @@ from airflow.utils.dates import days_ago
 
 from datetime import datetime, timedelta
 
-with DAG('init_hidayat',
+with DAG('init_isbakhul',
     schedule_interval="@once",
     start_date=datetime(2022, 7, 6)
 ) as dag:
@@ -17,22 +17,22 @@ with DAG('init_hidayat',
       
     ingest_orders = BashOperator(
         task_id='ingest_orders',
-        bash_command="""python3 /home/hadoop/airflow/dags/ingest/hidayat/ingest_orders.py {{ execution_date.format('YYYY-MM-DD') }}"""
+        bash_command="""python3 /home/hadoop/airflow/dags/ingest/isbakhul/ingest_orders.py {{ execution_date.format('YYYY-MM-DD') }}"""
     )
 
     to_datalake_orders = BashOperator(
         task_id='to_datalake_orders',
-        bash_command="""gsutil cp /home/hadoop/output/hidayat/orders/orders_{{ execution_date.format('YYYY-MM-DD') }}.csv gs://digitalskola-de-batch7/hidayat/staging/orders/"""
+        bash_command="""gsutil cp /home/hadoop/output/isbakhul/orders/orders_{{ execution_date.format('YYYY-MM-DD') }}.csv gs://digitalskola-de-batch7/isbakhul/staging/orders/"""
     )
 
     data_definition_orders = BashOperator(
         task_id='data_definition_orders',
-        bash_command="""bq mkdef --autodetect --source_format=CSV gs://digitalskola-de-batch7/hidayat/staging/orders/* > /home/hadoop/table_def/hidayat/orders.def"""
+        bash_command="""bq mkdef --autodetect --source_format=CSV gs://digitalskola-de-batch7/isbakhul/staging/orders/* > /home/hadoop/table_def/isbakhul/orders.def"""
     )
 
     to_dwh_orders = BashOperator(
         task_id='to_dwh_orders',
-        bash_command="""bq mk --external_table_definition=/home/hadoop/table_def/hidayat/orders.def de_7.hidayat_orders"""
+        bash_command="""bq mk --external_table_definition=/home/hadoop/table_def/isbakhul/orders.def de_7.isbakhul_orders"""
     )
 
     start >> ingest_orders >> to_datalake_orders >> data_definition_orders >> to_dwh_orders

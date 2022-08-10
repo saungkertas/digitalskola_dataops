268b3171701d785e4abc7a6c5e63e861e382e8af
Tri Purnomo <emailetri@gmail.com>
2022-08-10 13:24:29 +0700

    Update daily_tri.py

--- dags/daily_tri.py
+++ dags/daily_tri.py
@@ -40,17 +40,5 @@ with DAG(
         bash_command="""gsutil cp /root/output/tri/order_details/order_details_{{ execution_date.format('YYYY-MM-DD') }}.csv gs://digitalskola-de-batch7/tri/staging/order_details/"""
     )
 
-   
-    #To Dwh
-    to_dwh_orders = BashOperator(
-        task_id='to_dwh_orders',
-        bash_command="""bq mk --external_table_definition=/root/table_def/tri/orders.def de_7.tri_orders"""
-    )
-
-    to_dwh_order_details = BashOperator(
-        task_id='to_dwh_order_details',
-        bash_command="""bq mk --external_table_definition=/root/table_def/tri/order_details.def de_7.tri_order_details"""
-    )
-
-    start >> ingest_orders >> to_datalake_orders >> to_dwh_orders
-    start >> ingest_order_details >> to_datalake_order_details >> to_dwh_order_details
+    start >> ingest_orders >> to_datalake_orders
+    start >> ingest_order_details >> to_datalake_order_details

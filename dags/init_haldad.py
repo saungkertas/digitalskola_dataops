ef26e0f863b28b31e568d973628f4d55632c4a4f
almienava <haldad.almi@gmail.com>
2022-08-10 18:50:22 +0700

    edit

--- dags/init_haldad.py
+++ dags/init_haldad.py
@@ -36,7 +36,7 @@ for table in ['orders', 'order_details', 'products']:
     )
 
     to_dwh = BashOperator(
-        task_id='to_dwh_orders',
+        task_id='to_dwh_'+table,
         bash_command="""bq mk --external_table_definition=/root/table_def/haldad/{{ params.table_name }}.def de_7.haldad_{{ params.table_name }}""",
         params={'table_name':table}
     )

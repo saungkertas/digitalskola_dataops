5e4954d3094b5af24f7d50917fb891d2bfb22f4f
salmadhia00 <110456411+salmadhia00@users.noreply.github.com>
2022-08-11 17:11:52 +0700

    Add files via upload
    remove duplicate dag

--- dags/init_salma.py
+++ dags/init_salma.py
@@ -21,7 +21,7 @@ with DAG('init_salma',
         bash_command="""python3 /root/airflow/dags/ingest/salma/ingest_orders.py {{ execution_date.format('YYYY-MM-DD') }}"""
     )
 
-    ingest_orders = BashOperator(
+    ingest_order_details = BashOperator(
         task_id='ingest_order_details',
         bash_command="""python3 /root/airflow/dags/ingest/salma/ingest_order_details.py {{ execution_date.format('YYYY-MM-DD') }}"""
     )

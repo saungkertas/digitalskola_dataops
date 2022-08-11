cac1ce9de467d47be4b1229fcff96bf0f4556454
hildamei <57511960+hildamei@users.noreply.github.com>
2022-08-11 12:06:06 +0700

    pr daily_hilda

---
+++ dags/daily_hilda.py
@@ -0,0 +1,40 @@
+from airflow import DAG
+from airflow.operators.dummy import DummyOperator
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+from airflow.utils.dates import days_ago
+
+from datetime import datetime, timedelta
+
+with DAG('daily_hilda',
+    schedule_interval='0 0 * * *',
+    start_date=datetime(2022, 7, 1)       
+) as dag:
+
+    start = DummyOperator(
+        task_id='start'
+    )    
+
+    #orders  
+    ingest_orders = BashOperator(
+        task_id='ingest_orders',
+        bash_command="""python3 /root/airflow/dags/ingest/hilda/ingest_orders.py {{ execution_date.format('YYYY-MM-DD') }}"""
+    )
+
+    to_datalake_orders = BashOperator(
+        task_id='to_datalake_orders',
+        bash_command="""gsutil cp /root/output/hilda/orders/orders_{{ execution_date.format('YYYY-MM-DD') }}.csv gs://digitalskola-de-batch7/hilda/staging/orders/"""
+    )
+
+    #order_details
+    ingest_order_details = BashOperator(
+        task_id='ingest_order_details',
+        bash_command="""python3 /root/airflow/dags/ingest/hilda/ingest_order_details.py {{ execution_date.format('YYYY-MM-DD') }}"""
+    )
+    to_datalake_order_details = BashOperator(
+        task_id='to_datalake_order_details',
+        bash_command="""gsutil cp /root/output/hilda/order_details/order_details_{{ execution_date.format('YYYY-MM-DD') }}.csv gs://digitalskola-de-batch7/hilda/staging/order_details/"""
+    )
+
+    start >> ingest_orders >> to_datalake_orders
+    start >> ingest_order_details >> to_datalake_order_details

3b2046944a7b0f294ac02666ea3bb03db6deab91
Nur Indrasari <107334434+Indrasnur@users.noreply.github.com>
2022-08-11 19:56:38 +0800

    Update daily_sari.py

--- dags/daily_sari.py
+++ dags/daily_sari.py
@@ -8,7 +8,7 @@ from datetime import datetime, timedelta
 tabel_list = ['orders','order_details']
 
 with DAG('daily_sari',
-    schedule_interval='@daily',
+    schedule_interval='0 0 * * *',
     start_date=datetime(2022, 8, 1)       
 ) as dag:
 
@@ -32,13 +32,13 @@ for tb in tabel_list:
 
     data_definition = BashOperator(
         task_id='data_definition_'+tb,
-        bash_command="""bq mkdef --autodetect --source_format=CSV gs://digitalskola-de-batch7/sari/staging/{{params.tb}}/* > /root/table_def/sari/{{params.tb}}}.def"""
+        bash_command="""bq mkdef --autodetect --source_format=CSV gs://digitalskola-de-batch7/sari/staging/{{params.tb}}/* > /root/table_def/sari/{{params.tb}}}.def""",
         params = {'tb': tb}
     )
 
     to_dwh = BashOperator(
         task_id='dwh_'+tb,
-        bash_command="""bq mk --external_table_definition=/root/table_def/sari/{{params.tb}}.def de_7.sari_{{params.tb}}"""
+        bash_command="""bq mk --external_table_definition=/root/table_def/sari/{{params.tb}}.def de_7.sari_{{params.tb}}""",
         params = {'tb': tb}
     )
     start >> ingest >> to_datalake >> data_definition >> to_dwh

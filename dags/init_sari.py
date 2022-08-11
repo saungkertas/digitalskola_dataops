01cc7fed4ef12a6192ac04dc853bab1217e09154
Nur Indrasari <107334434+Indrasnur@users.noreply.github.com>
2022-08-11 19:39:32 +0800

    Update init_sari.py

--- dags/init_sari.py
+++ dags/init_sari.py
@@ -33,11 +33,13 @@ for tb1 in tabel_list1:
     data_definition1 = BashOperator(
         task_id='data_definition_'+tb1,
         bash_command="""bq mkdef --autodetect --source_format=CSV gs://digitalskola-de-batch7/sari/staging/{{params.tb1}}/* > /root/table_def/sari/{{params.tb1}}}.def"""
+        params = {'tb1': tb1}
     )
 
     to_dwh1 = BashOperator(
         task_id='dwh_'+tb1,
         bash_command="""bq mk --external_table_definition=/root/table_def/sari/{{params.tb1}}.def de_7.sari_{{params.tb1}}"""
+        params = {'tb1': tb1}
     )
     
     start >> ingest1 >> to_datalake1 >> data_definition1 >> to_dwh1
@@ -58,11 +60,13 @@ for tb2 in tabel_list2:
     data_definition2 = BashOperator(
         task_id='data_definition_'+tb2,
         bash_command="""bq mkdef --autodetect --source_format=CSV gs://digitalskola-de-batch7/sari/staging/{{params.tb2}}/* > /root/table_def/sari/{{params.tb2}}}.def"""
+        params = {'tb2': tb2}
     )
 
     to_dwh2 = BashOperator(
         task_id='dwh_'+tb2,
         bash_command="""bq mk --external_table_definition=/root/table_def/sari/{{params.tb2}}.def de_7.sari_{{params.tb2}}"""
+        params = {'tb2': tb2}
     )
 
     start >> ingest2 >> to_datalake2 >> data_definition2 >> to_dwh2
